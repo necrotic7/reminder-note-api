9bbe3ac88006b22e86dc5882bbb657a16064bddc
ZivWu <ziv.wu@clinico.com.tw>
2025-08-04 13:43:48 +0800

    fix: 修正取得reminders回傳內容

--- internal/models/reminders.model.go
+++ internal/models/reminders.model.go
@@ -7,15 +7,15 @@ import (
 )
 
 type ReminderModel struct {
-	ID         bson.ObjectID       `bson:"_id,omitempty"`
-	UserID     string              `bson:"userId"`
-	Title      string              `bson:"title"`
-	Content    string              `bson:"content"`
-	Frequency  EnumRemindFrequency `bson:"frequency"`
-	RemindTime RemindTime          `bson:"remindTime"`
-	Deleted    bool                `bson:"deleted"`
-	CreatedAt  time.Time           `bson:"createdAt"`
-	UpdatedAt  time.Time           `bson:"updatedAt"`
+	ID         bson.ObjectID       `bson:"_id,omitempty" json:"id"`
+	UserID     string              `bson:"userId" json:"userId"`
+	Title      string              `bson:"title" json:"title"`
+	Content    string              `bson:"content" json:"content"`
+	Frequency  EnumRemindFrequency `bson:"frequency" json:"frequency"`
+	RemindTime RemindTime          `bson:"remindTime" json:"remindTime"`
+	Deleted    bool                `bson:"deleted" json:"deleted"`
+	CreatedAt  time.Time           `bson:"createdAt" json:"createdAt"`
+	UpdatedAt  time.Time           `bson:"updatedAt" json:"updatedAt"`
 }
 
 type EnumRemindFrequency string
@@ -29,12 +29,12 @@ const (
 )
 
 type RemindTime struct {
-	Year    *int `bson:"year,omitempty"`
-	Month   *int `bson:"month,omitempty"`
-	Date    *int `bson:"date,omitempty"`
-	Weekday *int `bson:"weekday,omitempty"`
-	Hour    *int `bson:"hour,omitempty"`
-	Minute  *int `bson:"minute,omitempty"`
+	Year    *int `bson:"year,omitempty" json:"year"`
+	Month   *int `bson:"month,omitempty" json:"month"`
+	Date    *int `bson:"date,omitempty" json:"date"`
+	Weekday *int `bson:"weekday,omitempty" json:"weekday"`
+	Hour    *int `bson:"hour,omitempty" json:"hour"`
+	Minute  *int `bson:"minute,omitempty" json:"minute"`
 }
 
 type InsertReminderParams struct {
